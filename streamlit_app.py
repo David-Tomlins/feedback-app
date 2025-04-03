1cfb0a15b9d838954b35c6c83f631378a994905d
David-Tomlins <135724003+David-Tomlins@users.noreply.github.com>
2025-04-03 11:55:27 +0000

    Customised steamlit app and updated dependencies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Welcome to my new Stemalit app for Ometis feedback")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This is a simple example."
 )
