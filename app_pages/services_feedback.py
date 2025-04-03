702f153eee348173733185cae719ea2f9aa3f3d8
David-Tomlins <135724003+David-Tomlins@users.noreply.github.com>
2025-04-03 16:26:43 +0000

    built different pages and used token based routing

---
+++ app_pages/services_feedback.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+def render():
+    st.header("Services Feedback Form")
+
+    # Feedback form fields
+    name = st.text_input("Your Name", value="Name Surname", disabled=True)
+    company = st.text_input("Company (if relevant)", value="A Company", disabled=True)
+    email = st.text_input("Your Email", value="name.surname@company.com" ,disabled=True)
+    project = st.text_input("Project", value="Migrate Qlik Apps from PowerBI", disabled=True)
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.header("Mandatory question")
+    nps = st.slider("Would you recommend Ometis Consulting Services to a friend or colleague in the future?", 0, 10)
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.header("Optional questions")
+    overall = st.slider("Considering content, delivery, and materials, how would you rate the training overall?", 0, 10)
+    objectives = st.slider("Did the training meet your expected objectives?", 0, 10)
+    materials = st.slider("Did the training materials effectively support your learning?", 0, 10)
+    balance = st.slider("Was the balance between instructor-led activities and exercises appropriate?", 0, 10)
+    knowledge = st.slider("Was the instructor was knowledgeable on the subject?", 0, 10)
+    manner = st.slider("Did the instructor deliver the training in a clear and engaging manner?", 0, 10)
+    pace = st.slider("Was the pace of the training was suitable?", 0, 10)
+    questions = st.slider("Was there adequate time allocated for questions and discussions?", 0, 10)
+
+
+    comments = st.text_area("Are there specific aspects of the training you believe could be improved? Please provide details.")
+
+    if st.button("Submit Feedback"):
+        if nps == 0:
+            st.error("Please provide a rating for the mandatory question between 1 and 10 (0 is no response)")
+        else:
+            st.success("Thank you for your feedback!")
+
+
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import importlib
+
+# Collapse sidebar and hide toggle
+st.set_page_config(initial_sidebar_state='collapsed')
+st.markdown("""
+    <style>
+        [data-testid="stSidebar"], [data-testid="collapsedControl"] {
+            display: none !important;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
 
 # URL of the Ometis logo
@@ -7,34 +18,26 @@ logo_url = "https://ometis.co.uk/hs-fs/hubfs/ometis%20orange%20transparent-1.png
 # Display the Ometis logo
 st.image(logo_url, width=200)
 
-st.title("Education Feedback Form")
+st.title("Ometis Feedback Application")
 
-# Feedback form fields
-name = st.text_input("Your Name")
-email = st.text_input("Your Email")
-course = st.selectbox("Course Attended", ["Course 1", "Course 2", "Course 3"])
-st.markdown("<hr>", unsafe_allow_html=True)
-st.header("Mandatory question")
-nps = st.slider("Would you recommend Ometis Education to a friend or colleague in the future?", 0, 10)
-st.markdown("<hr>", unsafe_allow_html=True)
-st.header("Optional questions")
-overall = st.slider("Considering content, delivery, and materials, how would you rate the training overall?", 0, 10)
-objectives = st.slider("Did the training meet your expected objectives?", 0, 10)
-materials = st.slider("Did the training materials effectively support your learning?", 0, 10)
-balance = st.slider("Was the balance between instructor-led activities and exercises appropriate?", 0, 10)
-knowledge = st.slider("Was the instructor was knowledgeable on the subject?", 0, 10)
-manner = st.slider("Did the instructor deliver the training in a clear and engaging manner?", 0, 10)
-pace = st.slider("Was the pace of the training was suitable?", 0, 10)
-questions = st.slider("Was there adequate time allocated for questions and discussions?", 0, 10)
+token = st.query_params.get("token")
 
+TOKEN_ROUTES ={
+    "abcd1234":"education",
+    "xyz789": "services"
+}
 
-comments = st.text_area("Are there specific aspects of the training you believe could be improved? Please provide details.")
+ROUTES ={
+    "education": "app_pages.education_feedback" ,
+    "services": "app_pages.services_feedback"
+}
 
-if st.button("Submit Feedback"):
-    if nps == 0:
-        st.error("Please provide a rating for the mandatory question between 1 and 10 (0 is no response)")
-    else:
-        st.success("Thank you for your feedback!")
+if token in ROUTES:
+    module = importlib.import_module(ROUTES[TOKEN_ROUTES[token]])
+    module.render()
+else:
+    st.header("Sorry")
+    st.markdown("Thank you for taking the time to provide feedback — it really helps us improve the service we deliver. Unfortunately, something has gone wrong. Please get in touch with your contact at Ometis, and they’ll be happy to help resolve the issue.")
 
 
 
