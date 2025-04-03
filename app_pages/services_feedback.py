372603619f25a70df207b58f8146cecf1e6374cb
David-Tomlins <135724003+David-Tomlins@users.noreply.github.com>
2025-04-03 19:40:31 +0000

    started writing out the connection to SF]

--- app_pages/services_feedback.py
+++ app_pages/services_feedback.py
@@ -1,33 +1,50 @@
 import streamlit as st
 
+consultants = [
+    {"id": 1, "name": "Consultant A"},
+    {"id": 2, "name": "Consultant B"}
+    # Add more consultants as needed
+]
+
+feedback_data = {}
+
 def render():
     st.header("Services Feedback Form")
 
     # Feedback form fields
     name = st.text_input("Your Name", value="Name Surname", disabled=True)
-    company = st.text_input("Company (if relevant)", value="A Company", disabled=True)
+    company = st.text_input("Company", value="A Company", disabled=True)
     email = st.text_input("Your Email", value="name.surname@company.com" ,disabled=True)
     project = st.text_input("Project", value="Migrate Qlik Apps from PowerBI", disabled=True)
     st.markdown("<hr>", unsafe_allow_html=True)
-    st.header("Mandatory question")
+    st.header("Project feedback")
+    st.markdown("We sincerely appreciate you taking the time to provide us with your valuable feedback. Your insights are crucial in helping us enhance our services and better meet your needs. Thank you for contributing to our continuous improvement.")
+    st.markdown("Please score us between 1-10 (0 = no response)")
     nps = st.slider("Would you recommend Ometis Consulting Services to a friend or colleague in the future?", 0, 10)
     st.markdown("<hr>", unsafe_allow_html=True)
-    st.header("Optional questions")
-    overall = st.slider("Considering content, delivery, and materials, how would you rate the training overall?", 0, 10)
-    objectives = st.slider("Did the training meet your expected objectives?", 0, 10)
-    materials = st.slider("Did the training materials effectively support your learning?", 0, 10)
-    balance = st.slider("Was the balance between instructor-led activities and exercises appropriate?", 0, 10)
-    knowledge = st.slider("Was the instructor was knowledgeable on the subject?", 0, 10)
-    manner = st.slider("Did the instructor deliver the training in a clear and engaging manner?", 0, 10)
-    pace = st.slider("Was the pace of the training was suitable?", 0, 10)
-    questions = st.slider("Was there adequate time allocated for questions and discussions?", 0, 10)
+    st.header("Optional questions on your consultants")
+    
+    st.subheader("Please rate your consultant(s):")
+    st.markdown("1: Very poor to 5: Excellent  (0 = no response)")
+    for consultant in consultants:
+        with st.expander(f"Feedback for:", expanded=True):
+            feedback = {}
+            st.subheader(f"{consultant['name']}")
+            feedback['quality'] = st.slider("Quality of work", 0, 5, key=f"quality_{consultant['id']}")
+            feedback['communication'] = st.slider("Communication", 0, 5, key=f"communication_{consultant['id']}")
+            feedback['technical'] = st.slider("Technical capability and advice", 0, 5, key=f"technical_{consultant['id']}")
+            feedback['adaptaility'] = st.slider("Adaptability", 0, 5, key=f"adaptability_{consultant['id']}")
+            feedback['problemSolving'] = st.slider("Problem Solving", 0, 5, key=f"problem solving_{consultant['id']}")
+            feedback['comment'] = st.text_area(f"Are there specific comments you want to make about {consultant['name']}? Please provide details.", key=f"comments_{consultant['id']}")
+            feedback_data[consultant['id']] = feedback
+   
 
 
-    comments = st.text_area("Are there specific aspects of the training you believe could be improved? Please provide details.")
+    comments = st.text_area("Are there any other aspects that you believe could be improved? Please provide details.")
 
     if st.button("Submit Feedback"):
         if nps == 0:
-            st.error("Please provide a rating for the mandatory question between 1 and 10 (0 is no response)")
+            st.error("Please provide a rating for the question ""Would you recommend Ometis Consulting Services to a friend or colleague in the future?"" (0 is no response)")
         else:
             st.success("Thank you for your feedback!")
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,25 @@
 import streamlit as st
 import importlib
+import os
+import snowflake.connector
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
 
 # Collapse sidebar and hide toggle
 st.set_page_config(initial_sidebar_state='collapsed')
+
+# Retrieve Snowflake connection parameters from environment variables
+snowflake_user = os.getenv("SF_USERNAME")
+snowflake_account = os.getenv("SF_ACCOUNT")
+snowflake_private_key = os.getenv("SF_PRIVATE_KEY")
+snowflake_warehouse = "GENERAL_USE_WH"
+snowflake_database = "OMETIS_APPLICATIONS"
+snowflake_schema = "FEEDBACK_APP"
+
+
+st.header(snowflake_user)
+
+
 st.markdown("""
     <style>
         [data-testid="stSidebar"], [data-testid="collapsedControl"] {
@@ -32,12 +49,12 @@ ROUTES ={
     "services": "app_pages.services_feedback"
 }
 
-if token in ROUTES:
+if token in TOKEN_ROUTES:
     module = importlib.import_module(ROUTES[TOKEN_ROUTES[token]])
     module.render()
 else:
     st.header("Sorry")
-    st.markdown("Thank you for taking the time to provide feedback — it really helps us improve the service we deliver. Unfortunately, something has gone wrong. Please get in touch with your contact at Ometis, and they’ll be happy to help resolve the issue.")
+    st.markdown("Thank you for taking the time to provide feedback — it really helps us improve the service we deliver. Unfortunately, something has gone wrong and it looks like there is an issue with your token. Please get in touch with your contact at Ometis, and they’ll be happy to help resolve the issue.")
 
 
 
